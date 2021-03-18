3647163536be589f0e4b2a45d80878f5b819e26e
mapper895 <mapper.895.mp@gmail.com>
2021-03-18 17:41:45 -0600

    React Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import Home from "./pages";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SigninPage from "./pages/singin";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/sign-in" component={SigninPage} exact />
+      </Switch>
+    </Router>
   );
 }
 
