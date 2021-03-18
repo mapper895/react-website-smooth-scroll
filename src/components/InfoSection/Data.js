147d363ea6b5058944d2062b2a3e6627d1c836f2
mapper895 <mapper.895.mp@gmail.com>
2021-03-18 15:57:18 -0600

    Info Section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,19 @@
+import Img1 from "../../img/svg-3.svg";
+
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: Img1,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
