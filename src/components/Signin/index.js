47f5c0c18e9e43c9675795dc04b83aac61970f82
mapper895 <mapper.895.mp@gmail.com>
2021-03-19 11:40:02 -0600

    Sign In Component

---
+++ src/components/Signin/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Container,
+  Form,
+  FormButtom,
+  FormContent,
+  FormH2,
+  FormInput,
+  FormLabel,
+  FormWrap,
+  Icon,
+  Text,
+} from "./SigninElements";
+
+const SignIn = () => {
+  return (
+    <>
+      <Container>
+        <FormWrap>
+          <Icon to="/">dolla</Icon>
+          <FormContent>
+            <Form action="#">
+              <FormH2>Sign in to your account</FormH2>
+              <FormLabel htmlFor="for">Email</FormLabel>
+              <FormInput type="email" required />
+              <FormLabel htmlFor="for">Password</FormLabel>
+              <FormInput type="password" required />
+              <FormButtom type="submit">Continue</FormButtom>
+              <Text>Forgot password</Text>
+            </Form>
+          </FormContent>
+        </FormWrap>
+      </Container>
+    </>
+  );
+};
+
+export default SignIn;
--- src/pages/singin.js
+++ src/pages/singin.js
@@ -1,10 +1,11 @@
 import React from "react";
+import SignIn from "../components/Signin";
 
 const SigninPage = () => {
   return (
-    <div>
-      <h1>Sign in Page</h1>
-    </div>
+    <>
+      <SignIn />
+    </>
   );
 };
 
